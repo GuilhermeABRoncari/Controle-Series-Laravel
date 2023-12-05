6757386d2c9665208bec98a1e80570be5c00aebc
GuilhermeABRoncari <guilhermeabroncari@hotmail.com>
2023-12-04 21:26:21 -0300

    Adiciona capa base para todos os titulos sem capa

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -35,7 +35,10 @@ class SeriesController extends Controller
 
     public function store(SeriesFormRequest $request)
     {
-        $coverPath = (string) $request->file('cover')->store('series_cover', 'public');
+        $coverPath = $request->hasFile('cover')
+            ? $request->file('cover')->store('series_cover', 'public') 
+            : 'series_cover/base.jpg';
+    
         $request->setCoverPath($coverPath);
 
         $series = $this->repository->add($request);
